5ca018254068f41e46a73f0f41d064b52933d2fc
Adil Patel <adilpatel@me.com>
2018-09-14 15:18:56 +0200

    Initial Commit

---
+++ MetalPBR macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MetalPBR macOS
+//
+//  Created by Adil Patel on 14/09/2018.
+//  Copyright © 2018 Adil Patel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
